0b72a12f1401aa5477dcc51e012927401f930c74
AndrewCMartin <andrewcm@utexas.edu>
2016-12-22 00:11:42 -0600

    Cleaned up code, adding comments to MainActivity

--- app/src/main/java/com/example/android/binauralbeatsapp/MainActivity.java
+++ app/src/main/java/com/example/android/binauralbeatsapp/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.android.binauralbeatsapp;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,10 +10,14 @@ import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final static int SAMPLE_RATE = 8000;
-    private final static int TONE_DURATION = 1;
+    private final static int SAMPLE_RATE = 8000; // in Hz
+    private final static int TONE_DURATION = 1; // in seconds
 
-    AudioTrack audioTrack;
+    private AudioTrack audioTrack;
+
+    /** Initialize the AudioTrack object and volume control
+     * @param savedInstanceState
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,20 +25,19 @@ public class MainActivity extends AppCompatActivity {
         audioTrack =  new AudioTrack(AudioManager.STREAM_MUSIC, SAMPLE_RATE,
                 AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT,
                 SAMPLE_RATE * TONE_DURATION * 4, AudioTrack.MODE_STATIC);
-        int maxVolume = 100;
-        int curVolume = 100;
+        setupVolumeControl();
+    }
+
+    // Links volume bar to audio track
+    private void setupVolumeControl() {
         SeekBar volControl = (SeekBar)findViewById(R.id.volumeBar);
-        volControl.setMax(maxVolume);
-        volControl.setProgress(curVolume);
+        volControl.setMax(100);
+        volControl.setProgress(100);
         volControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onStopTrackingTouch(SeekBar arg0) {
-            }
-
+            public void onStopTrackingTouch(SeekBar arg0) {}
             @Override
-            public void onStartTrackingTouch(SeekBar arg0) {
-            }
-
+            public void onStartTrackingTouch(SeekBar arg0) {}
             @Override
             public void onProgressChanged(SeekBar arg0, int volume, boolean arg2) {
                 audioTrack.setVolume(((float)volume)/100);
@@ -43,90 +45,52 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
-
     public void playTheta(View view) {
-        genTone(300,304,true);
+        playTones(300,304);
     }
     public void playGamma(View view) {
-        genTone(400,410,true);
+        playTones(400,410);
     }
     public void playDelta(View view) {
-        genTone(600,601,true);
+        playTones(600,601);
     }
 
-
+    // Stops the audio and clears the buffer
     public void stop(View view) {
         audioTrack.pause();
         audioTrack.flush();
     }
 
-
-    private void genTone(double frequencyL, double frequencyR, boolean play) {
-        int sampleRate = SAMPLE_RATE;              // a number
-        int duration = TONE_DURATION;
-        double dnumSamples = duration * sampleRate;
-        dnumSamples = Math.ceil(dnumSamples);
-        int numSamples = (int) dnumSamples;
+    /**
+     * Creates and plays a sine wave tone with different frequencies in each channel.
+     * @param frequencyL The frequency (in Hz) of the tone to be played in the left ear
+     * @param frequencyR The frequency (in Hz) of the tone to be played in the right ear
+     */
+    private void playTones(double frequencyL, double frequencyR) {
+        int numSamples = TONE_DURATION * SAMPLE_RATE;
         double sampleL[] = new double[numSamples];
         double sampleR[] = new double[numSamples];
-        byte generatedSnd[] = new byte[2 * numSamples];
-        byte generatedStereo[] = new byte[4 * numSamples];
-
-        audioTrack.flush();
-
+        byte generatedStereoSamples[] = new byte[4 * numSamples];
         for (int i = 0; i < numSamples; ++i) {      // Fill the sample array
-            sampleL[i] = Math.sin(frequencyR * 2 * Math.PI * i / (sampleRate));
-            sampleR[i] = Math.sin(frequencyL * 2 * Math.PI * i / (sampleRate));
+            sampleR[i] = Math.sin(frequencyR * 2 * Math.PI * i / (SAMPLE_RATE));
+            sampleL[i] = Math.sin(frequencyL * 2 * Math.PI * i / (SAMPLE_RATE));
         }
-
-        // convert to 16 bit pcm sound array
+        // Converts to 16 bit pcm sound array
         // assumes the sample buffer is normalized.
-        // convert to 16 bit pcm sound array
-        // assumes the sample buffer is normalised.
-        int idx = 0;
-        int i = 0 ;
-
-        int ramp = numSamples * 0 / 100 ;                                    // Amplitude ramp as a percent of sample count
-
-
-        for (i = 0; i< ramp; ++i) {                                     // Ramp amplitude up (to avoid clicks)
-            // Ramp up to maximum
-            final short valL = (short) ((sampleL[i] * Short.MAX_VALUE * i/ramp));
-            final short valR = (short) ((sampleR[i] * Short.MAX_VALUE * i/ramp));
+        for (int idx = 0, i = 0; i < numSamples; ++i) {
+            // Max amplitude for the samples
+            final short valL = (short) (sampleR[i] * Short.MAX_VALUE);
+            final short valR = (short) (sampleL[i] * Short.MAX_VALUE);
             // in 16 bit wav PCM, first byte is the low order byte
-            generatedStereo[idx++] = (byte) (valL & 0x00ff);
-            generatedStereo[idx++] = (byte) ((valL & 0xff00) >>> 8);
-            generatedStereo[idx++] = (byte) (valR & 0x00ff);
-            generatedStereo[idx++] = (byte) ((valR & 0xff00) >>> 8);
+            generatedStereoSamples[idx++] = (byte) (valL & 0x00ff);
+            generatedStereoSamples[idx++] = (byte) ((valL & 0xff00) >>> 8);
+            generatedStereoSamples[idx++] = (byte) (valR & 0x00ff);
+            generatedStereoSamples[idx++] = (byte) ((valR & 0xff00) >>> 8);
         }
-
-
-        for (i = i; i< numSamples - ramp; ++i) {                        // Max amplitude for most of the samples
-            final short valL = (short) ((sampleL[i] * Short.MAX_VALUE));
-            final short valR = (short) ((sampleR[i] * Short.MAX_VALUE));
-            // in 16 bit wav PCM, first byte is the low order byte
-            generatedStereo[idx++] = (byte) (valL & 0x00ff);
-            generatedStereo[idx++] = (byte) ((valL & 0xff00) >>> 8);
-            generatedStereo[idx++] = (byte) (valR & 0x00ff);
-            generatedStereo[idx++] = (byte) ((valR & 0xff00) >>> 8);
-        }
-
-        for (i = i; i< numSamples; ++i) {                               // Ramp amplitude down
-            // Ramp down to zero
-            final short valL = (short) ((sampleL[i] * Short.MAX_VALUE * (numSamples-i)/ramp));
-            final short valR = (short) ((sampleR[i] * Short.MAX_VALUE * (numSamples-i)/ramp));
-            // in 16 bit wav PCM, first byte is the low order byte
-            generatedStereo[idx++] = (byte) (valL & 0x00ff);
-            generatedStereo[idx++] = (byte) ((valL & 0xff00) >>> 8);
-            generatedStereo[idx++] = (byte) (valR & 0x00ff);
-            generatedStereo[idx++] = (byte) ((valR & 0xff00) >>> 8);
-        }
-        audioTrack.write(generatedStereo, 0, generatedStereo.length);     // Load the track
-        audioTrack.reloadStaticData();
-        audioTrack.setLoopPoints(0,audioTrack.getBufferSizeInFrames(),-1);
-
+        audioTrack.flush(); // Clear AudioTrack
+        audioTrack.write(generatedStereoSamples, 0, generatedStereoSamples.length); // Send samples
+        audioTrack.reloadStaticData(); // Added to use Static Mode, needed for looping
+        audioTrack.setLoopPoints(0,audioTrack.getBufferSizeInFrames(),-1); // Infinite loop of the sample
         audioTrack.play();
-
     }
 }
