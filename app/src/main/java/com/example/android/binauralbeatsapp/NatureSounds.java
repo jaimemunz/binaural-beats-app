e40f4bc80cbcd3a5df845692292102f603bdca76
Jaime Munoz <jaimemunz@gmail.com>
2017-02-08 22:15:05 -0600

    Added start screen

--- app/src/main/java/com/example/android/binauralbeatsapp/NatureSounds.java
+++ app/src/main/java/com/example/android/binauralbeatsapp/NatureSounds.java
@@ -2,13 +2,20 @@ package com.example.android.binauralbeatsapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnTouchListener;
+import android.content.DialogInterface.OnClickListener;
 
 public class NatureSounds extends Activity {
+
+    Button rain_button = (Button) findViewById(R.id.rainforest_button);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nature_sounds);
     }
+
 
 }
